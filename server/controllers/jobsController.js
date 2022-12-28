22374a75bc4dafd296699adddea219f0a1dc3b5c
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2022-12-28 22:22:30 +0530

    added search functionality

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -1,5 +1,6 @@
 import Job from "../models/Job.js";
 import mongoose from "mongoose";
+import moment from "moment";
 import { StatusCodes } from "http-status-codes";
 import {
   BadRequestError,
@@ -23,11 +24,52 @@ const createJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId });
+  const { status, jobType, sort, search } = req.query;
 
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  // object that contains the queries that will be provided to mongoDB
+  const queryObject = {
+    createdBy: req.user.userId,
+  };
+
+  // add queries based on condition
+  if (status && status !== "all") {
+    queryObject.status = status;
+  }
+  if (jobType && jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
+  if (search) {
+    queryObject.position = { $regex: search, $options: "i" }; // to search using regular-expressions NOT exact text
+  }
+
+  // NO AWAIT
+  let result = Job.find(queryObject);
+
+  // chain sort conditions
+  if (sort === "latest") {
+    result = result.sort("-createdAt"); // Descending
+  }
+  if (sort === "oldest") {
+    result = result.sort("createdAt"); // Ascending
+  }
+  if (sort === "a-z") {
+    result = result.sort("position"); // Ascending
+  }
+  if (sort === "z-a") {
+    result = result.sort("-position"); // Descending
+  }
+
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  const jobs = await result;
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const updateJob = async (req, res) => {
@@ -92,8 +134,33 @@ const showStats = async (req, res) => {
     interview: stats.interview || 0,
     declined: stats.declined || 0,
   };
-  let monthlyApplications = [];
-  
+
+  // data for chart
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } }, // sort from latest to oldest
+    { $limit: 6 },
+  ]);
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1) // as mongoDB uses months(1-12) not (0-11)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
+
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
